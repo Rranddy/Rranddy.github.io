f6c5a1cac0d4c914e8935eb655ffaab9431c4b09
Rranddy <randywong1029@gmail.com>
2019-07-23 13:58:14 +0800

    updated videography page
    -removed the videos and link it using URL

--- Videography.php
+++ Videography.php
@@ -32,50 +32,56 @@ include "includes/header.php";
 <!-- video layout -->
 <div class="video">
 	<h1>Petronas MotorsportKL streetmeet</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/motorsportKL.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/6F7D16z5GSo" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen>
+    </iframe>
 </div>
 
 <div class="video">
-	<h1>cars & kopi</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/car&kopi.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+	<h1>Cars & Kopi</h1>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/HMiSGr19z8o" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
 </div>
 
 <div class="video">
 	<h1>GXS x Kuantan Team</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/GXS2.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/XoU640Uv088" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
 </div>
 
 <div class="video">
-	<h1>KL Live New Year Countdown</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/KL Live.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+	<h1>KL Live 2019 New Year Countdown</h1>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/HnAO9d6LpwI" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
 </div>
 
 <div class="video">
 	<h1>GXS First Capsule Collection Teaser</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/GXS1.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/6lzcR3aF4yM" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
 </div>
 
 <div class="video">
-	<h1>outliner</h1>
-    <video width="400" height="320" controls>
-      <source src="./video/outliner.mp4" type="video/mp4">
-      <source src="movie.ogg" type="video/ogg">
-    </video>
+	<h1>Outliner</h1>
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/MCKcA3fNLVE" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
 </div>
 
 </body>
